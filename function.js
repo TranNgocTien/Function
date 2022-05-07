e8ed6157e045d705c300b82756b11fb017730db0
Tiến Trần <tientran@Tiens-MacBook-Air.local>
2022-05-07 14:05:06 +0700

    palindrome

--- function.js
+++ function.js
@@ -6,4 +6,18 @@ function reverseNumber(number){
     return +arr.reverse().join('');
 }
 console.log(reverseNumber(592075));
-console.log(3333);
+
+
+
+
+// 2. Write a JavaScript function that checks whether a passed string is palindrome or not?
+// A palindrome is word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.
+const palindrome= function (str){
+    const reverseArr= str.split('').reverse().join('');
+    return str===reverseArr? true : false;
+}
+
+console.log(palindrome('anna'));
+console.log(palindrome('civic'));
+console.log(palindrome('kayak'));
+console.log(palindrome('hello'));
